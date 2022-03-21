e3abc3171b94e333c992277f673d79fb5b5f1f0b
dexcabigting <dexcabigting1234@gmail.com>
2022-03-22 01:43:31 +0800

    Updated BookSeeder.php

--- database/seeders/BookSeeder.php
+++ database/seeders/BookSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\Book;
+
 class BookSeeder extends Seeder
 {
     /**
@@ -14,6 +16,39 @@ class BookSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $books = [
+            [
+                'title' => 'Tiktik',
+                'author' => 'Dingdong Dantes',
+                'category' => 'Bold',
+                'description' => 'Rated 18+',
+                'publishing_house' => 'MCC',
+                'publishing_date' => now(),
+                'created_at' => now(),
+                'updated_at' => now() 
+            ],
+            [
+                'title' => 'Betlog',
+                'author' => 'Sir Bamba',
+                'category' => 'IOT',
+                'description' => 'wala hahaha',
+                'publishing_house' => 'MIS',
+                'publishing_date' => now(),
+                'created_at' => now(),
+                'updated_at' => now() 
+            ],
+            [
+                'title' => 'OJT',
+                'author' => 'Maam Zabala',
+                'category' => 'maternity leave',
+                'description' => 'moa signing',
+                'publishing_house' => 'IBCE',
+                'publishing_date' => now(),
+                'created_at' => now(),
+                'updated_at' => now() 
+            ]
+        ];
+
+        Book::insert($books);
     }
 }
