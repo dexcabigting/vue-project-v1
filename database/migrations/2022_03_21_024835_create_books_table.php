6b08d73e921e3bbba21c9c90633736d3bbaaa631
dexcabigting <dexcabigting1234@gmail.com>
2022-03-21 11:18:25 +0800

    Created migration file for books table

---
+++ database/migrations/2022_03_21_024835_create_books_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->string('author', 100);
+            $table->string('category', 100);
+            $table->string('publishing_house', 100);
+            $table->date('publishing_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
