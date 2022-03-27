34b5f7826343d6231de77b44c8d5a28a45e44236
dexcabigting <dexcabigting1234@gmail.com>
2022-03-27 14:45:48 +0800

    Modified method name

--- tests/Feature/BookStoreEndpointTest.php
+++ tests/Feature/BookStoreEndpointTest.php
@@ -18,7 +18,7 @@ class BookStoreEndpointTest extends TestCase
 
     // Test to Pass
 
-    public function testIfEndpointExistsWithValidData()
+    public function testIfEndpointExistsWithValidFormInputs()
     {
         $this->withoutExceptionHandling();
 
@@ -40,7 +40,9 @@ class BookStoreEndpointTest extends TestCase
         $response->assertStatus(201);
     }
 
-    public function testIfEndpointRejectsMissingInputs()
+    // Test to Fail
+    
+    public function testIfEndpointRejectsMissingOneOrMoreFormInputs()
     {
         $book = [
             'title' => null,
@@ -56,109 +58,4 @@ class BookStoreEndpointTest extends TestCase
         $response->assertInvalid(['title', 'author', 'description', 'category', 'publishing_house', 'publishing_date']);
         $response->assertStatus(302);
     }
-
-    public function testIfEndpointRejectsInvalidTitle()
-    {
-        $book = [
-            'title' => 'Samp',
-            'author' => 'Mongmong Barcos',
-            'description' => 'Tujongs',
-            'category' => 'Politics',
-            'publishing_house' => 'GenSan',
-            'publishing_date' => now()
-        ];
-
-        $response = $this->post('/api/books', $book);
-
-        $response->assertInvalid(['title']);
-        $response->assertStatus(302);
-    }
-
-    public function testIfEndpointRejectsInvalidAuthor()
-    {
-        $book = [
-            'title' => 'Sample Book',
-            'author' => 'Mong',
-            'description' => 'Tujongs',
-            'category' => 'Politics',
-            'publishing_house' => 'GenSan',
-            'publishing_date' => now()
-        ];
-
-        $response = $this->post('/api/books', $book);
-
-        $response->assertInvalid(['author']);
-        $response->assertStatus(302);
-    }
-
-    
-    public function testIfEndpointRejectsInvalidDescription()
-    {
-        $book = [
-            'title' => 'Sample Book',
-            'author' => 'Mongmong Marcos',
-            'description' => 'Tujo',
-            'category' => 'Politics',
-            'publishing_house' => 'GenSan',
-            'publishing_date' => now()
-        ];
-
-        $response = $this->post('/api/books', $book);
-
-        $response->assertInvalid(['description']);
-        $response->assertStatus(302);
-    }
-
-    public function testIfEndpointRejectsInvalidCategory()
-    {
-        $book = [
-            'title' => 'Sample Book',
-            'author' => 'Mongmong Marcos',
-            'description' => 'Tujongs',
-            'category' => 'Poli',
-            'publishing_house' => 'GenSan',
-            'publishing_date' => now()
-        ];
-
-        $response = $this->post('/api/books', $book);
-
-        $response->assertInvalid(['category']);
-        $response->assertStatus(302);
-    }
-
-    public function testIfEndpointRejectsInvalidPublishingHouse()
-    {
-        $book = [
-            'title' => 'Sample Book',
-            'author' => 'Mongmong Marcos',
-            'description' => 'Tujongs',
-            'category' => 'Politics',
-            'publishing_house' => 'GenS',
-            'publishing_date' => now()
-        ];
-
-        $response = $this->post('/api/books', $book);
-
-        $response->assertInvalid(['publishing_house']);
-        $response->assertStatus(302);
-    }
-
-    public function testIfEndpointRejectsInvalidPublishingDate()
-    {
-        $book = [
-            'title' => 'Sample Book',
-            'author' => 'Mongmong Marcos',
-            'description' => 'Tujongs',
-            'category' => 'Politics',
-            'publishing_house' => 'GenSan',
-            'publishing_date' => 232
-        ];
-
-        $response = $this->post('/api/books', $book);
-
-        $response->assertInvalid(['publishing_date']);
-        $response->assertStatus(302);
-    }
-
-    // Test to Fail
 }
